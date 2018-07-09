cbabd44783b14cf6ef919637de9a21a9b66fcc59
xach <zironside@hotmail.com>
2018-07-09 03:11:58 +0100

    work on merge concept

--- src/DataMerge.py
+++ src/DataMerge.py
@@ -4,15 +4,17 @@ class DataMerge:
     def __init__(self, db1: Database, db2: Database):
         self.db1 = db1
         self.db2 = db2
+        self.forginKeys = []
 
     def merge(self):
         # merge data
         self.findRelations(self.db1) 
+        self.diffDatabases(self.db1, self.db2)
+        print(self.forginKeys)
         return False
 
     def connect(self, db: Database):
         if db.database != '':
-            print(db.database)
             return pymysql.connect(host=db.host,
                                 user=db.user,
                                 password=db.password,
@@ -32,24 +34,26 @@ class DataMerge:
             with dbc.cursor() as cursor:
                 cursor.execute("SHOW TABLES")
                 results = cursor.fetchall()
-                forginKeys = []
                 for result in results:
-                    forginKeys.append(self.getForginKey(db, result["Tables_in_"+db.database]))
-                print(results)
+                    keys = self.getForginKey(db, result["Tables_in_"+db.database])
+                    if len(keys) > 0:
+                        for key in keys:
+                            self.forginKeys.append(key)
         finally:
             dbc.close()
         
     def getForginKey(self, db: Database, table):
+
         query = """
             SELECT
                 TABLE_NAME,
                 COLUMN_NAME,
-                CONSTRAINT_NAME,   -- <<-- the one you want! 
+                CONSTRAINT_NAME,
                 REFERENCED_TABLE_NAME,
                 REFERENCED_COLUMN_NAME
             FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
             WHERE
-                REFERENCED_TABLE_SCHEMA = '{database}'
+                REFERENCED_TABLE_SCHEMA = '{database}' AND
                 REFERENCED_TABLE_NAME = '{table}';
             """.format(table=table, database=db.database)
         dbc = self.connect(Database(db.host, db.user, db.password, ''))
@@ -57,7 +61,26 @@ class DataMerge:
             with dbc.cursor() as cursor:
                 cursor.execute(query)
                 results = cursor.fetchall()
-                print(query)
-                print(results)
+                if len(results) > 0:
+                    forginKeys = []
+                    for result in results:
+                        forginKeys.append({"table": result["TABLE_NAME"], "forgin_table": result["REFERENCED_TABLE_NAME"], "column": result["COLUMN_NAME"]})
+                    return forginKeys
         finally:
             dbc.close()
+        return []
+
+        def diffDatabases(self, db1: Database, db2: Database):
+            dbc1 = self.connect(db1) 
+            dbc2 = self.connect(db2)
+
+            try:
+                with dbc1.cursor as cursor:
+                    cursor.execute("SHOW TABLES")
+                    results = cursor.fetchall()
+                    for result in results:
+                        data = self.getTableData(result["Tables_in_"+db1.database])
+                        self.compareData(data, db2)
+
+            finally:
+                dbc1.close()
